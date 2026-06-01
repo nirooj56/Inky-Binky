52becc3b696193a0e881620ca2ff704c1a5040b1
Bista Nirooj <nirooj56@gmail.com>
2026-06-01 18:19:53 +0545

    Version 0.2.1
    Added the Pause Menu for Desktop, Mobile and Web.

---
+++ README.md
@@ -0,0 +1,13 @@
+![Inky Binky](https://github.com/nirooj56/Inky-Binky/blob/main/Icon.svg)
+<h1>Inky Binky</h1>
+
+Step into a world of raw sketches and rapid-fire combat. In Inky Binky, you control a uniquely designed character fighting to stay alive against encroaching enemies on an infinite, hand-drawn battlefield.
+
+Designed with a clean, sketchy aesthetic, the game focuses on pure movement and survival. Whether you’re playing on a PC with a keyboard or on the go with your mobile device, the experience is optimized for smooth, responsive action.
+Technical Specs (Important for itch.io)
+
+    Platforms: Windows, Android, Web (HTML5).
+    Controls: WASD/Arrows for Movement (PC), On-screen Touch Controls (Mobile).
+    Engine: Powered by Godot 4.
+
+Play it here: <a href="https://nirooj.itch.io/inky-binky"><b>Inky Binky</a></b>
---
+++ scripts/pause_menu.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+@onready var mobile_pause_button: Button = $MobilePauseButton
+@onready var color_rect: ColorRect = $ColorRect2
+@onready var quit_button: Button = $ColorRect2/VBoxContainer/Quit
+
+func _ready() -> void:
+	color_rect.hide() # Hides Pause Menu overlay on boot
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	# Handle the Mobile Pause Button visibility
+	if OS.has_feature("mobile") or OS.has_feature("android"):
+		mobile_pause_button.show()
+	else:
+		mobile_pause_button.hide()
+
+	# updates the Quit button text to the Main Menu on Web and Mobile
+	if OS.has_feature("web") or OS.has_feature("mobile") or OS.has_feature("android"):
+		quit_button.text = "Main Menu"
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		toggle_pause()
+	
+func toggle_pause():
+	var new_pause_state = !get_tree().paused
+	get_tree().paused = new_pause_state
+	color_rect.visible = new_pause_state
+	# Smooth mouse visibility handling
+	if new_pause_state:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # Show cursor for menu selection
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)  # Hide it during gameplay
+	
+	if OS.has_feature("mobile") or OS.has_feature("android"):
+		mobile_pause_button.visible = !new_pause_state
+	
+func _on_resume_pressed() -> void:
+	toggle_pause()
+
+func _on_quit_pressed() -> void:
+	get_tree().paused = false
+		
+	if OS.has_feature("web") or OS.has_feature("mobile") or OS.has_feature("android"):
+			get_tree().change_scene_to_file("res://scenes/game_screen.tscn")
+	else:
+			get_tree().quit()
+
+func _on_mobile_pause_button_pressed() -> void:
+	mobile_pause_button.release_focus()
+	toggle_pause()
+
